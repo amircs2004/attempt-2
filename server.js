818c408941c69eeeb6a8d13568d47176004c066e
amircs2004 <saidimahdiamir@gmail.com>
2026-07-27 20:14:35 +0100

    adding bdd connection

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const PORT = 5000 
+const bddconnectionTest = require('./routes/bdd.route')
 app.use(
   cors({
-    origin: true,
+    origin: '*',
     methods : ['GET' , 'DELETE' , 'POST' , 'PUT' , 'OPTIONS'] ,
     credentials: true, 
      allowedHeaders : ['Content-Type' , 'Authorization' , 'Cookie']
@@ -14,7 +15,9 @@ app.use(
 );
 app.use(cookieParser());
 app.use(express.json());
- 
+ app.use('/api' , bddconnectionTest)
+
+//npm run dev 
 if (process.env.NODE_ENV !== "production") {
   app.listen(PORT , ()=> () => console.log(`Server running on port ${PORT}`))
 
