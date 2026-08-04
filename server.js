0044305dae08113eab91468a306221a584c6bce5
amircs2004 <saidimahdiamir@gmail.com>
2026-08-04 17:36:11 +0100

    scaling up to prevent a hacker from crashing my server

--- server.js
+++ server.js
@@ -7,7 +7,24 @@ const PORT = 5000
 const bddconnectionTest = require('./routes/bdd.route')
 const authControllers = require('./routes/auth.route')
 const ClientProductRoutes = require('./routes/product.route')
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
 
+//limitting the number of requests to prevent brute force attacks 
+app.use(helmet()); 
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+  standardHeaders: true, // Return rate limit info in `RateLimit-*` headers
+  legacyHeaders: false, // Disable `X-RateLimit-*` headers
+  message: {
+    status: 429,
+    message: "Too many requests from this IP, please try again after 15 minutes.",
+  },
+});
+
+// Apply rate limiting strictly to all API routes
+app.use("/api", apiLimiter);
 
 
 app.use(
@@ -19,7 +36,8 @@ app.use(
   })
 );
 app.use(cookieParser());
-app.use(express.json());
+// 4. Payload Size Limiting (Prevents attackers from sending massive 50MB JSON payloads)
+app.use(express.json({ limit: "10kb" }));
  app.use('/api' , bddconnectionTest)
  app.use('/api' , authControllers)
  app.use('/api' , ClientProductRoutes)
